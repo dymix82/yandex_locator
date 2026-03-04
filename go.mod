e342f24a0ec591d7203b8846d6ced7df0e941027
dymix82 <d.gavva@gmail.com>
2026-03-04 21:07:59 +0300

    убрал unsafe , упростил userinfo

--- go.mod
+++ go.mod
@@ -2,6 +2,15 @@ module locator
 
 go 1.25.0
 
-require golang.org/x/sys v0.41.0
+require (
+	github.com/iamacarpet/go-win64api v0.0.0-20240507095429-873e84e85847
+	github.com/kardianos/service v1.2.4
+	golang.org/x/sys v0.41.0
+)
 
-require github.com/kardianos/service v1.2.4
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/cabbie v1.0.2 // indirect
+	github.com/google/glazier v0.0.0-20211029225403-9f766cca891d // indirect
+	github.com/scjalliance/comshim v0.0.0-20190308082608-cf06d2532c4e // indirect
+)
--- modules/userinfo/userinfo.go
+++ modules/userinfo/userinfo.go
@@ -5,8 +5,8 @@ package userinfo
 import (
 	"os"
 	"strings"
-	"syscall"
-	"unsafe"
+
+	wapi "github.com/iamacarpet/go-win64api"
 )
 
 // GetActiveUsername возвращает имя пользователя, вошедшего в систему (активного или отключённого).
@@ -20,86 +20,9 @@ func GetActiveUsername() string {
 		return username
 	}
 
-	// Загружаем библиотеки
-	wtsapi32 := syscall.NewLazyDLL("wtsapi32.dll")
-	procWTSEnumerateSessionsW := wtsapi32.NewProc("WTSEnumerateSessionsW")
-	procWTSQuerySessionInformationW := wtsapi32.NewProc("WTSQuerySessionInformationW")
-	procWTSFreeMemory := wtsapi32.NewProc("WTSFreeMemory")
-
-	const (
-		WTS_CURRENT_SERVER_HANDLE = 0
-		WTSUserName               = 5
-		// Состояния сессии
-		WTSActive       = 0
-		WTSConnected    = 1
-		WTSConnectQuery = 2
-		WTSShadow       = 3
-		WTSDisconnected = 4
-		WTSIdle         = 5
-		WTSListen       = 6
-		WTSReset        = 7
-		WTSDown         = 8
-		WTSInit         = 9
-	)
-
-	var sessionInfo *byte
-	var count uint32
-
-	ret, _, _ := procWTSEnumerateSessionsW.Call(
-		WTS_CURRENT_SERVER_HANDLE,
-		0,
-		1,
-		uintptr(unsafe.Pointer(&sessionInfo)),
-		uintptr(unsafe.Pointer(&count)),
-	)
-	if ret == 0 {
-		return "unknown"
-	}
-	defer procWTSFreeMemory.Call(uintptr(unsafe.Pointer(sessionInfo)))
-
-	type WTS_SESSION_INFO struct {
-		SessionID      uint32
-		WinStationName *uint16
-		State          uint32
-	}
-
-	// Массив допустимых состояний – любое, кроме служебных (Listen, Idle и т.д.)
-	// На практике нам подойдут Active, Connected, Disconnected
-	validStates := map[uint32]bool{
-		WTSActive:       true,
-		WTSConnected:    true,
-		WTSDisconnected: true,
-	}
-
-	for i := uint32(0); i < count; i++ {
-		item := (*WTS_SESSION_INFO)(unsafe.Pointer(
-			uintptr(unsafe.Pointer(sessionInfo)) + uintptr(i)*unsafe.Sizeof(WTS_SESSION_INFO{}),
-		))
-
-		if !validStates[item.State] {
-			continue
-		}
-
-		var userName *uint16
-		var bytes uint32
-		ret, _, _ = procWTSQuerySessionInformationW.Call(
-			WTS_CURRENT_SERVER_HANDLE,
-			uintptr(item.SessionID),
-			WTSUserName,
-			uintptr(unsafe.Pointer(&userName)),
-			uintptr(unsafe.Pointer(&bytes)),
-		)
-		if ret == 0 || userName == nil {
-			continue
-		}
-		name := syscall.UTF16ToString((*[1 << 20]uint16)(unsafe.Pointer(userName))[:bytes/2])
-		procWTSFreeMemory.Call(uintptr(unsafe.Pointer(userName)))
-
-		// Исключаем системные имена
-		if name != "" && !strings.HasSuffix(name, "$") &&
-			name != "SYSTEM" && name != "LOCAL SERVICE" && name != "NETWORK SERVICE" {
-			return name
-		}
+	users, _ := wapi.ListLoggedInUsers()
+	if len(users) > 0 {
+		return users[0].Username
 	}
 
 	// Если ничего не нашли, пробуем через переменные окружения ещё раз (на случай, если раньше отсекли из-за $)
